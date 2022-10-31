d9b15472d3722dbc12bcb9df0c1ee23098513c26
EliasEstevez <elias.ismael.estevez@estudiantat.upc.edu>
2022-10-31 12:17:41 +0100

    simplificación vad_docopt.h inicio

--- scripts/run_vad.sh
+++ scripts/run_vad.sh
@@ -6,7 +6,8 @@
 # Write here the name and path of your program and database
 DIR_P2=$HOME/PAV/P2
 DB=$DIR_P2/db.v4
-CMD="$DIR_P2/bin/vad  --alfa1=$1"           #aqui meter en $1 el valor por defecto no?
+CMD=$DIR_P2/bin/vad
+#CMD="$DIR_P2/bin/vad  --alfa1=$1"           #aqui meter en $1 el valor por defecto no?
 #CMD="$DIR_P2/bin/vad  --alfa2=$2"
 
 for filewav in $DB/*/*wav; do
